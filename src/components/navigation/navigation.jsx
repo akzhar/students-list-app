0f43a436e4cbc9f7a715f397c44eb6bac91e9c7f
Akzhar <akzhar.taov@yandex.ru>
2021-03-12 00:01:43 +0300

    Добавляет компоненты Navigation

---
+++ src/components/navigation/navigation.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+
+const Navigation = () => (
+  <nav className="navigation">
+    <Link to="/">Назад к списку студентов</Link>
+  </nav>
+);
+
+export default Navigation;

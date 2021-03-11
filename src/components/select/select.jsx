92b0b2f508e8d9d51956347b95de2a862fdfde82
Akzhar <akzhar.taov@yandex.ru>
2021-03-12 00:01:09 +0300

    Дорабатывает компонент Select: исправляет ширину списка опций, добавляет иконку-стрелочку

--- src/components/select/select.jsx
+++ src/components/select/select.jsx
@@ -5,17 +5,18 @@ const Select = ({options, initialActive = ``, onActiveOptionChange = null}) => {
 
   const [activeOption, setActiveOption] = useState(initialActive || options[0]);
   const selectRef = useRef();
+  const selectOptionsRef = useRef();
 
   const onOptionSelection = (newActiveOption) => {
     if (onActiveOptionChange && activeOption !== newActiveOption) {
       onActiveOptionChange(newActiveOption);
     }
     setActiveOption(newActiveOption);
-    selectRef.current.classList.remove(`select__options--opened`);
   };
 
   const toggleSelectOptions = () => {
-    selectRef.current.classList.toggle(`select__options--opened`);
+    selectRef.current.classList.toggle(`select--opened`);
+    selectOptionsRef.current.classList.toggle(`select__options--opened`);
   };
 
   const handleSelectClick = () => toggleSelectOptions();
@@ -35,18 +36,31 @@ const Select = ({options, initialActive = ``, onActiveOptionChange = null}) => {
   };
 
   return <React.Fragment>
-    <span className="select" tabIndex="0" onClick={handleSelectClick} onKeyDown={handleSelectSpaceDown}>{activeOption}</span>
-    <ul className="select__options" ref={selectRef} onClick={handleOptionSelect} onKeyDown={handleOptionsSpaceDown}>
-      {options.map((option) => (
-        <li
-          key={option}
-          tabIndex="0"
-          className={`select__option ${(option === activeOption) && `select__option--active`}`}
-        >
-          {option}
-        </li>
-      ))}
-    </ul>
+    <div
+      className="select"
+      ref={selectRef}
+      tabIndex="0"
+      onClick={handleSelectClick}
+      onKeyDown={handleSelectSpaceDown}
+    >
+      {activeOption}
+      <ul
+        className="select__options"
+        ref={selectOptionsRef}
+        onClick={handleOptionSelect}
+        onKeyDown={handleOptionsSpaceDown}
+      >
+        {options.map((option) => (
+          <li
+            key={option}
+            tabIndex="0"
+            className={`select__option ${(option === activeOption) && `select__option--active`}`}
+          >
+            {option}
+          </li>
+        ))}
+      </ul>
+    </div>
   </React.Fragment>;
 };
 

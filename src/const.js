82856f0d65dfc38d73cecbfac9cd61e587eb3213
Akzhar <akzhar.taov@yandex.ru>
2021-03-11 21:42:15 +0300

    Уточняет ф-ции сортировки списка студентов

--- src/const.js
+++ src/const.js
@@ -1,9 +1,25 @@
 export const SORT_OPTIONS = [`Имя`, `Рейтинг`, `Возраст`, `Любимый цвет`];
 export const ActiveSortToCompareFunc = {
-  "Имя": (a, b) => a.name - b.name,
-  "Рейтинг": (a, b) => b.rating - a.rating,
-  "Возраст": (a, b) => b.age - a.age,
-  "Любимый цвет": (a, b) => b.favoriteColor - a.favoriteColor
+  "Имя": (a, b) => {
+    if (a.name > b.name) {
+      return -1;
+    }
+    if (a.name < b.name) {
+      return 1;
+    }
+    return 0;
+  },
+  "Рейтинг": (a, b) => a.rating - b.rating,
+  "Возраст": (a, b) => a.age - b.age,
+  "Любимый цвет": (a, b) => {
+    if (a.favoriteColor > b.favoriteColor) {
+      return -1;
+    }
+    if (a.favoriteColor < b.favoriteColor) {
+      return 1;
+    }
+    return 0;
+  }
 };
 export const AppRoute = {
   MAIN: `/`,

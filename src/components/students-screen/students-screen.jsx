ec202a0b5693f094ff8c68bb7cf964c82bfd2dad
Akzhar <akzhar.taov@yandex.ru>
2021-03-17 23:27:39 +0300

    Добавляет вид для пустого списка студентов

--- src/components/students-screen/students-screen.jsx
+++ src/components/students-screen/students-screen.jsx
@@ -1,20 +1,32 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import {connect} from 'react-redux';
 
 import Container from '../container/container.jsx';
 import Header from '../header/header.jsx';
 import Menu from '../menu/menu.jsx';
 import Students from '../students/students.jsx';
+import StudentsEmpty from '../students-empty/students-empty.jsx';
 
-const StudentsScreen = () => (
+const StudentsScreen = ({hasStudents}) => (
   <React.Fragment>
     <Header/>
     <main>
       <Container>
         <Menu/>
-        <Students/>
+        {hasStudents ? <Students/> : <StudentsEmpty/>}
       </Container>
     </main>
   </React.Fragment>
 );
 
-export default StudentsScreen;
+const mapStateToProps = (state) => ({
+  hasStudents: state.students.items.length !== 0
+});
+
+StudentsScreen.propTypes = {
+  hasStudents: PropTypes.bool.isRequired
+};
+
+export {StudentsScreen};
+export default connect(mapStateToProps, null)(StudentsScreen);

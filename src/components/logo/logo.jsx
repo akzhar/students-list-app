fe4cd2a48a912385e6cc23948133987b9dbcdeb7
Akzhar <akzhar.taov@yandex.ru>
2021-03-20 19:01:32 +0300

    Делает ссылку в логотипе шапки неактивной на главной странице

--- src/components/logo/logo.jsx
+++ src/components/logo/logo.jsx
@@ -1,15 +1,25 @@
 import React from 'react';
+import {useHistory} from 'react-router-dom';
 import {Link} from 'react-router-dom';
+import {AppRoute} from '../../const.js';
 
 import logo from '../../img/logo.svg';
 
-const Logo = () => (
-  <div className="logo">
+const Logo = () => {
+
+  const history = useHistory();
+  const isDisable = history.location.pathname === AppRoute.MAIN;
+
+  return <div className="logo">
     <img src={logo} alt="logo" width="42" height="42"/>
-    <Link to="/">
-      STUDENTS by <span>akzhar</span>
+    <Link
+      to={AppRoute.MAIN}
+      tabIndex={isDisable ? `-1` : `0`}
+      className={`logo__link ${isDisable && `logo__link--disable`}`}
+    >
+      STUDENTS <span className="logo__author">by <mark className="logo__name">akzhar</mark></span>
     </Link>
-  </div>
-);
+  </div>;
+};
 
 export default Logo;

c07a589d03c2a5bc470669d90d49f91785a70b72
Akzhar <akzhar.taov@yandex.ru>
2021-03-26 23:13:01 +0300

    Готовит клиенсткую часть к деплою: добавляет express.js для раздачи приложения, package.json, favicon

---
+++ public/index.js
@@ -0,0 +1,12 @@
+const path = require(`path`);
+const express = require(`express`);
+
+const PORT = process.env.PORT || 8080;
+
+const app = express();
+app.use(express.static(__dirname));
+
+app.get(`/*`, (_req, res) => {
+  res.sendFile(path.resolve(__dirname, `index.html`));
+});
+app.listen(PORT);

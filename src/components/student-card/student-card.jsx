b50cafd45a4edb566451759872c4ba8898700d0f
Akzhar <akzhar.taov@yandex.ru>
2021-04-01 00:39:02 +0300

    Исправляет баг с удалением каррточки студента

--- src/components/student-card/student-card.jsx
+++ src/components/student-card/student-card.jsx
@@ -1,30 +1,13 @@
-import React, {useRef} from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
-import {connect} from 'react-redux';
-import {ActionCreator} from '../../store/actions.js';
 import {studentTypes} from '../../prop-types/prop-types.jsx';
 import {getAgeDeclination} from '../../utils.js';
-import {Message} from '../../const.js';
 
 import FavColor from '../fav-color/fav-color.jsx';
 import ButtonDelete from '../button-delete/button-delete.jsx';
 
-const StudentsCard = ({student, showPopup, removeStudent}) => {
-
-  const cardRef = useRef();
-
-  const handleDeleteBtnClick = (evt) => {
-    const card = cardRef.current;
-    const studentId = evt.target.value;
-    const onSuccess = () => {
-      card.parentNode.removeChild(card);
-      showPopup(Message.OK.CARD_WAS_REMOVED);
-    };
-    const onFail = () => showPopup(Message.ERROR.CARD_WAS_NOT_REMOVED);
-    removeStudent(studentId, onSuccess, onFail);
-  };
-
-  return <li className="students__card card" ref={cardRef}>
+const StudentsCard = ({student, onDeleteBtnClick}) => (
+  <li className="students__card card">
     <img className="card__avatar" src={student.avatar} alt={student.name} width="40" height="40"/>
     <div className="card__col-center">
       <p className="card__name">{student.name}</p>
@@ -36,20 +19,13 @@ const StudentsCard = ({student, showPopup, removeStudent}) => {
       <p className="card__spec">{student.spec}</p>
       <p className="card__group">{student.group}</p>
     </div>
-    <ButtonDelete title={`Удалить карточку студента ${student.name}`} value={student.id} onClick={handleDeleteBtnClick}/>
-  </li>;
-};
-
-const mapDispatchToProps = (dispatch) => ({
-  showPopup: (message) => dispatch(ActionCreator.showPopup(message)),
-  removeStudent: (studentId, onSuccess, onFail) => dispatch(ActionCreator.removeStudent(studentId, onSuccess, onFail))
-});
+    <ButtonDelete title={`Удалить карточку студента ${student.name}`} value={student.id} onClick={() => onDeleteBtnClick(student.id)}/>
+  </li>
+);
 
 StudentsCard.propTypes = {
   student: PropTypes.shape(studentTypes).isRequired,
-  showPopup: PropTypes.func.isRequired,
-  removeStudent: PropTypes.func.isRequired
+  onDeleteBtnClick: PropTypes.func.isRequired
 };
 
-export {StudentsCard};
-export default connect(null, mapDispatchToProps)(StudentsCard);
+export default StudentsCard;
--- src/components/students/students.jsx
+++ src/components/students/students.jsx
@@ -1,13 +1,26 @@
-import React from 'react';
+import React, {useState} from 'react';
 import PropTypes from 'prop-types';
 import {studentTypes} from '../../prop-types/prop-types.jsx';
 import {connect} from 'react-redux';
-import {SEARCH_BY_FIELD, SortTypeToCompareFunc} from '../../const.js';
+import {ActionCreator} from '../../store/actions.js';
+import {SEARCH_BY_FIELD, SortTypeToCompareFunc, Message} from '../../const.js';
 
 import StudentCard from '../student-card/student-card.jsx';
 
-const Students = ({searchSubstring, sortType, students}) => (
-  <section className="students">
+const Students = ({searchSubstring, sortType, students, showPopup, removeStudent}) => {
+
+  const [studentCards, setStudentCards] = useState([...students]);
+
+  const handleDeleteBtnClick = (studentId) => {
+    const onSuccess = () => {
+      setStudentCards((cards) => cards.filter((card) => card.id !== studentId));
+      showPopup(Message.OK.CARD_WAS_REMOVED);
+    };
+    const onFail = () => showPopup(Message.ERROR.CARD_WAS_NOT_REMOVED);
+    removeStudent(studentId, onSuccess, onFail);
+  };
+
+  return <section className="students">
     <div className="students__header">
       <span>ФИО</span>
       <span>Специальность</span>
@@ -16,7 +29,7 @@ const Students = ({searchSubstring, sortType, students}) => (
       <span>Рейтинг</span>
     </div>
     <ul className="students__list">
-      {students
+      {studentCards
       .filter((student) => {
         // регистронезависимый поиск
         const fieldValue = student[SEARCH_BY_FIELD].toLowerCase();
@@ -25,11 +38,11 @@ const Students = ({searchSubstring, sortType, students}) => (
       })
       .sort(SortTypeToCompareFunc[sortType])
       .map((student) => (
-        <StudentCard key={student.id + student.name + student.age} student={student}/>
+        <StudentCard key={student.id + student.name + student.age} student={student} onDeleteBtnClick={handleDeleteBtnClick}/>
       ))}
     </ul>
-  </section>
-);
+  </section>;
+};
 
 const mapStateToProps = (state) => ({
   searchSubstring: state.active.searchSubstring,
@@ -37,10 +50,17 @@ const mapStateToProps = (state) => ({
   students: state.students.items
 });
 
+const mapDispatchToProps = (dispatch) => ({
+  showPopup: (message) => dispatch(ActionCreator.showPopup(message)),
+  removeStudent: (studentId, onSuccess, onFail) => dispatch(ActionCreator.removeStudent(studentId, onSuccess, onFail))
+});
+
 Students.propTypes = {
   searchSubstring: PropTypes.string.isRequired,
   sortType: PropTypes.string.isRequired,
-  students: PropTypes.arrayOf(PropTypes.shape(studentTypes)).isRequired
+  students: PropTypes.arrayOf(PropTypes.shape(studentTypes)).isRequired,
+  showPopup: PropTypes.func.isRequired,
+  removeStudent: PropTypes.func.isRequired
 };
 
-export default connect(mapStateToProps, null)(Students);
+export default connect(mapStateToProps, mapDispatchToProps)(Students);
